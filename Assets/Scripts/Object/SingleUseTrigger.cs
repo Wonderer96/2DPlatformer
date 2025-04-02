3766a862917c5f3ebf35f2ca038cda6025f2fb4b
Wonderer96 <87023779+Wonderer96@users.noreply.github.com>
2025-04-02 23:34:02 +0800

    阶段性上传

--- Assets/Scripts/Object/SingleUseTrigger.cs
+++ Assets/Scripts/Object/SingleUseTrigger.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 public class SingleUseTrigger : BaseTrigger
 {
     [Header("Flip Settings")]
     [Tooltip("是否允许连续触发")]
     public bool allowContinuousFlip = false;
 
+    public float cooldownTime = 2f; // 设置倒计时时间
+    private bool isCooldown = false; // 用来记录倒计时状态
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (activatorTags.Contains(other.tag))
+        if (activatorTags.Contains(other.tag) && !isCooldown)
         {
-           // if (allowContinuousFlip || !IsActivated)
-            {
-                IsActivated = !IsActivated;
-            }
+            // 开始倒计时
+            StartCoroutine(Cooldown());
+            IsActivated = !IsActivated;
         }
+    }
+
+    private IEnumerator Cooldown()
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(cooldownTime); // 等待指定的时间
+        isCooldown = false;
     }
 }
